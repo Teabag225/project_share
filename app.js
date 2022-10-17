d92b6666d748fc9674f1341ce1edff9f562b96c9
Teabag225 <taewook225@naver.com>
2022-10-17 14:28:33 +0900

    new repository for sharing the project

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 9090;
+
+app.set('view engine', 'ejs');
+app.use('/views', express.static(__dirname + '/views'));
+app.use('/static', express.static(__dirname + '/static'));
+
+app.get('/', function(request, response){
+    response.render('index')
+});
+
+app.listen(PORT, function(){
+    console.log(`Listening on port ${PORT}!`);
+});
