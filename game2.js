3aa6c5aee2abcf684a0c628856b3f144a766372e
Sujeong <nksj98@naver.com>
2022-10-21 09:51:54 +0900

    feat:game2 구현

---
+++ game2.js
@@ -0,0 +1,27 @@
+function slideUp() {
+  let card1 = document.getElementById("card1");
+  let card2 = document.querySelector(".box");
+  let cardPosition = document.createElement("div");
+  card1.classList.add("slideUp");
+
+  //   let btnN = document.createElement("button");
+  //   const BtnTextY = document.createTextNode("Y");
+  //   const BtnTextN = document.createTextNode("N");
+
+  //Y버튼
+
+  //버튼
+  //   btnY.appendChild(BtnTextY);
+  //   btnN.appendChild(BtnTextN);
+
+  //   cardPosition.append(btnY, btnN);
+  //   card2.append(card1, cardPosition);
+}
+
+// let btnY = document.querySelector(".btnimg");
+function btny() {
+  //   btnY.addEventListener("click", function () {
+  card1.classList.add("rotateCard");
+  //   cardPosition.parentNode.removeChild(cardPosition);
+  //   });
+}
