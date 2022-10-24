c0104b396b5d897fd36c6de3c8e69a4f10c46b4d
Teabag225 <taewook225@naver.com>
2022-10-24 09:31:11 +0900

    css styling & adjusted the search bar position

--- static/js/search.js
+++ static/js/search.js
@@ -1,14 +1,21 @@
-function search_animal() {
-    let input = document.getElementById('searchbar').value
-    input=input.toLowerCase();
-    let x = document.getElementsByClassName('card-title');
-
-    for (i = 0; i < x.length; i++) {
-        if (!x[i].innerHTML.toLowerCase().includes(input)){
-            x[i].style.display="none";
-        }
-        else {
-            x[i].style.display="list-item"
-        }
+const search = () =>{
+    const searchbox = document.getElementById("search-item").value.toUpperCase();
+    const storeitems = document.getElementById("product-list")
+    const product = document.querySelectorAll(".card")
+    const pname = storeitems.getElementsByTagName("p")
+  
+    for(var i = 0; i < pname.length; i++){
+      let match = product[i].getElementsByTagName('p')[0];
+  
+      if(match){
+       let textvalue = match.textContent || match.innerHTML
+       
+       if(textvalue.toUpperCase().indexOf(searchbox) > -1) {
+        product[i].style.display = "";
+       } else {
+        product[i].style.display = "none";
+  
+       }
+       }
+      }
     }
-}
